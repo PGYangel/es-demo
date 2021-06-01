e27b54e902a64900cd2188e752275b314d39be89
yerong <241761025@qq.com>
2021-06-01 09:57:25 +0800

    Iterator

--- static/demo/3_ES6异步编程与模块化/Iterator.js
+++ static/demo/3_ES6异步编程与模块化/Iterator.js
@@ -4,3 +4,57 @@
 主要供for...of消费
 一句话：不支持遍历的数据结构“可遍历”
 */
+
+// 具备Symbol.iterator类型的数据即可遍历
+let map = new Map()
+map.set('name', '张三')
+map.set('age', 18)
+map.set('school', '清华')
+let it = map[Symbol.iterator]()
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+
+// 原生具备Iterator接口的数据结构有：
+/* 
+Array
+Map
+Set
+String
+TypedArray
+函数的arguments对象
+NodeList对象
+*/
+
+// 可迭代协议：Symbol.iterator
+// 迭代器协议：return { next(){ return {value,done} } }
+let courses = {
+    allCourse: {
+        a: ['a', 'aa', 'aaa', 'aaaa'],
+        b: ['b', 'bb', 'bbb', 'bbbb'],
+        c: ['c', 'cc', 'ccc', 'cccc']
+    }
+}
+courses[Symbol.iterator] = function () {
+    let allCourse = this.allCourse
+    let keys = Reflect.ownKeys(allCourse)
+    let values = []
+    return {
+        next() {
+            if (!values.length) {
+                if (keys.length) {
+                    values = allCourse[keys[0]]
+                    keys.shift()
+                }
+            }
+            return {
+                done: !values.length,
+                value: values.shift()
+            }
+        }
+    }
+}
+for (let item of courses) {
+    console.log(item);
+}
